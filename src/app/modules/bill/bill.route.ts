8dd0f69963a8a808953c5c6296aae97bf3dd03e4
muntasirmoin <muntasirmoinchowdhury099@gmail.com>
2026-09-10 13:56:19 +0600

    add bill route

--- src/app/modules/bill/bill.route.ts
+++ src/app/modules/bill/bill.route.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { validateRequest } from "../../middlewares/validateRequest";
 import { generateBillValidation } from "./bill.validation";
-import { generateBill, listBills } from "./bill.controller";
+import { downloadReceipt, generateBill, listBills } from "./bill.controller";
 const router = Router();
 router.post(
   "/",
@@ -11,4 +11,11 @@ router.post(
   generateBill,
 );
 router.get("/", checkAuth("administrator", "manager", "counter"), listBills);
+
+router.get(
+  "/:id/receipt",
+  checkAuth("counter", "manager", "administrator"),
+  downloadReceipt,
+);
+
 export const BillRoutes = router;
